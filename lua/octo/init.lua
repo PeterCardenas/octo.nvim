3c2b431bf34be3f25f9a5018ef308c90a31472bf
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2026-03-16 20:21:14 -0700

    fix: octo buffer marked as modified

--- lua/octo/init.lua
+++ lua/octo/init.lua
@@ -407,6 +407,7 @@ function M.create_buffer(kind, obj, repo, create, hostname)
     octo_buffer:async_fetch_issues()
   end
   utils.clear_history()
+  vim.bo[bufnr].modified = false
   require("octo.polling").track_buffer(bufnr)
 end
 
